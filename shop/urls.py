e407353f510c641cb39099686d6a1237eb18fad9
Vinderblad7 <patkovskijtimofej@gmail.com>
2025-08-07 18:23:16 +0300

    added cart edited templates and fixed bugs

--- shop/urls.py
+++ shop/urls.py
@@ -5,9 +5,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart', include('cart.urls', namespace='cart')),
     path('', include('main.urls', namespace='main')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    
